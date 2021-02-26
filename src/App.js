7ea3720ba1833c04b8bab8a78b904fceddc9071d
JJFAO <jjfalonso@gmail.com>
2021-02-25 21:26:13 -0300

    primeros cambios ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [screen, setScreen] = useState('Home');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      {screen === 'Home' && <div>Home</div>}
+      {screen === 'Login' && <div>Login</div>}
+      {screen === 'Register' && <div>Registro</div>}
     </div>
   );
 }
