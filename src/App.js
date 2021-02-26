6bb7286810d83dcd8144acd48a2005d1a8b82013
JJFAO <jjfalonso@gmail.com>
2021-02-25 21:46:38 -0300

    Header con 3 vistas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import { useState } from 'react';
+import { Nav, Navbar } from 'react-bootstrap';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   const [screen, setScreen] = useState('Home');
   return (
     <div className="App">
-      
+      <>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link onClick={() => setScreen('Home')}>Home</Nav.Link>
+            <Nav.Link onClick={() => setScreen('Register')}>Registro</Nav.Link>
+            <Nav.Link onClick={() => setScreen('Login')}>Login</Nav.Link>
+          </Nav>
+        </Navbar>
+      </>
       {screen === 'Home' && <div>Home</div>}
       {screen === 'Login' && <div>Login</div>}
       {screen === 'Register' && <div>Registro</div>}
