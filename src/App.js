512f7f36fc4f31673622349e2a923f3fa16452e4
Ricky Moreno <ricky.gnr.moreno@gmail.com>
2021-03-04 21:43:34 -0300

    add react-router-dom, add routes /login and /register

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Register from './components/Register';
 import Login from './components/Login';
@@ -11,10 +12,20 @@ function App() {
 
     return (
         <div className="App">
-            <Header token={token} setScreen={setScreen} />
+            {/* <Header token={token} setScreen={setScreen} />
             {screen === 'Home' && <div>Home</div>}
             {screen === 'Login' && <Login setScreen={setScreen} setToken={setToken} />}
-            {screen === 'Register' && <Register setScreen={setScreen} setToken={setToken} />}
+            {screen === 'Register' && <Register setScreen={setScreen} setToken={setToken} />} */}
+            <Router>
+                <Switch>
+                    <Route path="/login">
+                        <Login />
+                    </Route>
+                    <Route path="/register">
+                        <Register />
+                    </Route>
+                </Switch>
+            </Router>
         </div>
     );
 }
