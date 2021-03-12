3c1f4f61916a3d63db82278131ac3f69f52c6e21
Ricky Moreno <ricky.gnr.moreno@gmail.com>
2021-03-11 21:49:50 -0300

    vista memes

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,13 +1,24 @@
 export default function Meme({ meme }) {
     const { creador, createdAt, imagen, titulo } = meme;
-    const fecha = new Date(createdAt);
+    const fecha = new Date(createdAt).toLocaleString();
 
     return (
-        <div>
-            <h3>{titulo}</h3>
-            <img src={imagen} alt="meme" />
-            <span className="text-muted">{fecha.toLocaleString()}</span>
-            <p>{creador.nombre}</p>
+        <div className="m-auto">
+            <div className="p-2 my-3" style={{ width: '30rem' }}>
+                <div style={{ fontSize: '0.7rem' }}>by {creador.nombre}</div>
+                <h3>{titulo}</h3>
+                <img src={imagen} alt="meme" className="img-fluid" />
+                <div className="d-flex justify-content-between m-2">
+                    <span style={{ fontSize: '0.7rem' }}>
+                        Publicado el: <strong>{fecha}</strong>
+                    </span>
+                    <img
+                        src="https://icongr.am/feather/heart.svg?size=128&color=ba1c1c"
+                        alt="like"
+                        width="25"
+                    />
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/Memes.jsx
+++ src/components/Memes.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Meme from './Meme';
+import { Container, Row } from 'react-bootstrap';
 
 export default function Memes() {
     const [memes, setMemes] = useState([]);
@@ -13,10 +14,12 @@ export default function Memes() {
     }, []);
 
     return (
-        <div>
-            {memes.map((m) => (
-                <Meme meme={m} key={m._id} />
-            ))}
-        </div>
+        <Container className="my-5">
+            <Row className="flex-column justify-content-center">
+                {memes.map((m) => (
+                    <Meme meme={m} key={m._id} />
+                ))}
+            </Row>
+        </Container>
     );
 }
